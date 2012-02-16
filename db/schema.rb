2d924309749756d8735760422147fe2707318066
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-02-15 21:03:11 -0300

    add task model and update show story view to list its tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120215230710) do
+ActiveRecord::Schema.define(:version => 20120215233735) do
 
   create_table "stories", :force => true do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "tasks", :force => true do |t|
+    t.string   "description"
+    t.integer  "story_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
