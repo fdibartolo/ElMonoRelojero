635e6ab8df2073d84ec4bac441e6ed0274fc425c
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-04-16 20:49:17 -0300

    do columns styling

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,4 +1,13 @@
 $(function(){
+	
+	/*calculate story width*/
+	$("[name=story_name]").each(function(index){
+		var number_of_letters = $(this).text().length;
+		var width = (number_of_letters * 12) + 20
+		$(this).closest(".story").css("width", width);
+	});
+	
+	/*set drag and drop prefs*/
 	$(".story").draggable({
 		revert: "invalid",
 		revertDuration: 250,
@@ -7,6 +16,8 @@ $(function(){
 	});
 	
 	var color;
+	var inprogress_color = "#F5F6CE";
+	var done_color = "#CEF6D8";
 	$("[id*=drop]").droppable({
 		accept: ".story",
 		over: function (e, ui) {
@@ -17,8 +28,11 @@ $(function(){
 			$(this).css("background-color", color);
 		},
 		drop: function(e, ui) {
-			/*var story_name = $(ui.draggable).find("h2").text();
-			$(this).text(story_name);*/
+			if ($(this).attr("id").indexOf("drop_inprogress_") != -1 ) {
+				$(ui.draggable).css("background-color", inprogress_color);
+			} else {
+				$(ui.draggable).css("background-color", done_color);				
+			}
 			$(this).css("background-color", color);
 		}
 	});
